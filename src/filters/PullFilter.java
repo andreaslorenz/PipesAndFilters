ec242ce3a24e2583e62bb3898599c2d5ca445845
Andreas Lorenz <lorenz.and@gmail.com>
2012-10-29 12:41:57 +0100

    PullFilter

--- src/filters/PullFilter.java
+++ src/filters/PullFilter.java
@@ -1,5 +1,21 @@
 package filters;
 
+import pipes.IPullPipe;
+
 public abstract class PullFilter<out, in> implements IFilter {
 
+	boolean outputFinished;
+	out outPut;
+	IPullPipe<in, out> pullPipe;
+	
+	public out read(){
+		
+		return null;
+	}
+	
+	public void process(in input){
+		
+		
+	}
+	
 }
